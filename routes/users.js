1e17aa6f60bffb2a2b3c4fd11fd7f54979286d1e
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-05-27 00:21:08 +0530

    Adding Sessions

--- routes/users.js
+++ routes/users.js
@@ -5,45 +5,82 @@ const bcrypt = require('bcryptjs');
 // User Model
 const User = require('../models/User');
 
-// @route   POST api/users
-// @desc    Register new user
-// @access  Public
-router.post('/', (req, res) => {
+
+
+// // Resgitering a new user 
+router.post('/register', (req, res) => {
     const { name, email, password } = req.body;
 
     if(!name || !email || !password) return res.status(400).json({ msg: 'Please enter all fields' });
 
-
     // Check for existing user
     User.findOne({ email })
-        .then(user => {
-            if(user) return res.status(400).json({ msg: 'User already exists' });
+    .then(user => {
+        if(user) return res.status(400).json({ msg: 'User already exists' });
 
-            const newUser = new User({ name, email,password });
+        const newUser = new User({ name, email,password });
 
-            // Create salt & hash
-            bcrypt.genSalt(10, (err, salt) => {
-                if(err) throw err;
+        bcrypt.hash(password, 10)
+        .then(function(hash) {
+            newUser.password = hash;
 
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if(err) throw err;
-
-                    newUser.password = hash;
-                    newUser.save()
-                        .then(user => {
-                            res.json({
-                                user: {
-                                    id: user.id,
-                                    name: user.name,
-                                    email: user.email
-                                }
-                            });
-                        }
-                    );
-                })
+            newUser.save()
+            .then(user => {
+                req.session.userId = user._id;
+                res.json({ 
+                    msg: 'New User created', 
+                    user: {
+                        name: user.name,
+                        email: user.email
+                    }
+                });
             });
+        });
+    });
+});    
+
+
+
+// Logging in a existing user 
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) return res.status(400).json({ msg: 'Please enter all fields' });
+
+    var user;
+    // Check for existing user
+    User.findOne({ email: email })
+    .then(user => {
+        if(!user) return res.status(400).json({ msg: 'User does not exists' });
+        
+        if(bcrypt.compareSync(password, user.password)){
+            req.session.userId = user._id;
+            return res.status(200).json({ msg: 'Authentiation Succeeded'});
+        } else {
+            return res.status(400).json({ msg: 'Authentication failed, invalid username or password.' });
         }
-    );
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ msg: 'Authentication failed, invalid username or password.' });
+    });
 });
+
+
+
+// Logout the user
+router.get('/logout', function(req, res) {
+    if (req.session) {
+        req.session.destroy(function(err) {
+            if(err) {
+                return res.status(500).json({msg: "Please try again"});
+            } else {
+                res.clearCookie('session');
+                return res.redirect('/');
+            }
+        });
+    }
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+var session = require('express-session');
 
 const app = express();
 
-// To parse JSON bodies into JS objects
 app.use(bodyParser.json());
+
 
 
 // Connecting to MongoDB
@@ -17,9 +18,24 @@ mongoose.connect(db, {
   .catch(err => console.log(err));
 
 
+
+// Using sessions for tracking Logins
+app.use(session({
+  name: 'session',
+  secret: 'sessionSecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: (1000*60*60),
+    sameSite: true,
+    secure: false,
+  }
+}));
+
+
 // Use Routes
 app.use('/api/blog', require('./routes/blogs'));
-app.use('/api/users', require('./routes/users'));
+app.use('/api/user', require('./routes/users'));
 
 // Routes
 app.get('/', (req, res) => {
